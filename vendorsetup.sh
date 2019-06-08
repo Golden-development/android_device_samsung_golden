8445290da6f2574ee92e6b79fa68a527c52dc6b7
FacuM <facumo.fm@gmail.com>
2019-06-08 22:32:52 +0000

    vendorsetup: force vendor patch.
    Change-Id: I2dae8ec90334d0d780ecf71f38ac4d46b6638892

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,5 @@
+echo 'VENDOR PATCH OK'
+patch -p1 --dry-run -s -f < device/samsung/golden/patches/vendor-lineage.patch 2> /dev/null > /dev/null || true
 add_lunch_combo lineage_golden-eng
 add_lunch_combo lineage_golden-userdebug
 add_lunch_combo lineage_golden-user
