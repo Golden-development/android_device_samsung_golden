74b4ef94a9a41d17443400de7c4bb882b6161445
FacuM <facumo.fm@gmail.com>
2019-06-09 04:00:52 +0000

    vendorsetup: automatically patch during environment setup.
    Change-Id: Ie4ef21975099a70b607d16791792a94e77e37bcc

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,15 @@
-echo 'VENDOR PATCH OK'
-patch -p1 --dry-run -s -f < device/samsung/golden/patches/vendor-lineage.patch 2> /dev/null > /dev/null || true
+echo 'Patching...'
+PRE="$PWD"
+DEVICE_PATCHES='device/samsung/golden/patches'
+for f in $(ls "$DEVICE_PATCHES")
+do
+ NEXT_PATH=$(printf "$f" | sed 's/.patch//' | sed 's/-/\//g') # process next path from patch filename
+ echo 'Applying "'"$f"'" to '"$NEXT_PATH"'...'
+ cd "$NEXT_PATH"
+ git apply "$PRE"'/'"$DEVICE_PATCHES"'/'"$f" 2> /dev/null > /dev/null || true
+ cd "$PRE"
+done
+echo 'Patch completed.'
 add_lunch_combo lineage_golden-eng
 add_lunch_combo lineage_golden-userdebug
 add_lunch_combo lineage_golden-user
